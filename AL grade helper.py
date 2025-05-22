1e61e3c8155ad7facab6dc38ea7de00f73f9e122
Produlx <dulsathlenovo12@gmail.com>
2025-05-22 22:16:47 +0530

    fix some bugs in arrows

--- AL grade helper.py
+++ AL grade helper.py
@@ -2,16 +2,16 @@ print("HI, i hope you have good day..")
 
 marks_user = int(input("Enter your marks: "))
 
-if marks_user == 75:
+if marks_user >= 75:
     print("A")
 else:
-    if marks_user == 65:
+    if marks_user >= 65:
         print("B")
     else:
-        if marks_user == 55:
+        if marks_user >= 55:
             print("C")
         else:
-            if marks_user == 35:
+            if marks_user >= 35:
                 print("S")
             else:
                 if marks_user < 35:
